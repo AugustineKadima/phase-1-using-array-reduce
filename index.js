37efc6bafd5565302c5f33399c6573960bdbe5d5
AugustineKadima <kadimasam@gmail.com>
2022-02-27 17:23:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+function totalBatteries(){
+    let total = 0
+    for(let battery of batteryBatches){
+        total += battery
+    }
+
+    return total
+}
